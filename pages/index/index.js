9e04ffd62683bd1292d722bbcb7297e3851e171b
yuxiang <1780768307@qq.com>
2019-12-24 18:33:44 +0800

    初始化页面和首页

---
+++ pages/index/index.js
@@ -0,0 +1,23 @@
+Page({
+  data: {
+    img: [
+    'http://b.hiphotos.baidu.com/image/h%3D300/sign=05b297ad39fa828bce239be3cd1e41cd/0eb30f2442a7d9337119f7dba74bd11372f001e0.jpg',
+    'http://img1.imgtn.bdimg.com/it/u=1734073965,524448280&fm=26&gp=0.jpg',
+    'http://attach.bbs.miui.com/forum/201111/21/205700txzuacubbcy91u99.jpg'
+    ],
+    text:[
+     '代办事项',
+     '代办事项1',
+     '代办事项2',
+     '代办事项3',
+     '代办事项4',
+     '代办事项5',
+     '代办事项6',
+    ],
+    indicatorDots: true,
+    vertical: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 500
+  },
+})
